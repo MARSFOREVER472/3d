57b9c2c06812bf6c3467ffd173b80063abebeb49
Marcelo Andrés <marcelo.lam@alumnos.ucn.cl>
2023-04-29 01:41:32 -0400

    unity 3d objects through scene collisions
    We worked through all the components that it had been difficult for me to carry out at the moment of executing in the scene to be able to collide objects with the plane

--- Práctica 3d/Assets/Scripts/HelloWorld.cs
+++ Práctica 3d/Assets/Scripts/HelloWorld.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
+    public float flotantePublico = 0.1f;
+    private float flotantePrivado = 0.1f;
+    [SerializeField] private float flotantePrivadoSerializeField = 0.1f;
+    private bool isWorking = true;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,65 @@ public class HelloWorld : MonoBehaviour
     void Update()
     {
         Debug.Log("Hola Mundo muchas veces");
+    }
+
+
+    // Detecta cuando una colision se acaba de producir
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Estoy colisionando con un objeto cualquiera");
+
+        if (other.gameObject.CompareTag("Suelo")) 
+        {
+            Debug.Log("Hey, Colisione con el suelo");
+        }
+    }
+
+    // Detecta cuando una colision se produce constantemente
+    private void OnCollisionStay(Collision other)
+    {
+        Debug.Log("Me mantengo colisionando con un objeto cualquiera");
+
+        if (other.gameObject.CompareTag("Suelo"))
+        {
+            Debug.Log("Me mantengo colisionando con el suelo");
+        }
+
+    }
+
+    // Detecta cuando una colision deje de producirse
+    private void OnCollisionExit(Collision other)
+    {
+        Debug.Log("Sali de la colision con un objeto cualquiera");
+
+        if (other.gameObject.CompareTag("Suelo"))
+        {
+            Debug.Log("Deje de colisionar con el suelo");
+        }
+
+    }
+
+    // Gatilla de un evento cuando se produce una colision
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Suelo")) {
+            Debug.Log("Hice una colision con el trigger");
+        }
+    }
+
+    // Gatilla de un evento cuando se mantiene una colision
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Suelo")) {
+            Debug.Log("Estoy manteniendome colisionando con el trigger");
+        }
+    }
+
+    // Gatilla de un evento cuando se termina una colision
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Suelo")) {
+            Debug.Log("Deje de hacer la colision con el trigger");
+        }
     }
 }
